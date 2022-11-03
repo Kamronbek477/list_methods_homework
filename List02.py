dc55046756ffc2b78f624eb5eb7d339a797ffa30
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-11-04 01:10:48 +0500

    List02.py

--- List02.py
+++ List02.py
@@ -8,4 +8,7 @@ def main(fruits,x,i):
     Returns:
         list: return answer
     """
-    return
+    
+    fruits.insert(i,x)
+    return fruits
+print(main(['vivo','samsung','iphone','RedMi'],'nokia',3))
