d22b15aff29f4509a92d7cde3fe9a709e4c61a7a
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-11-04 01:27:29 +0500

    List05.py

--- List05.py
+++ List05.py
@@ -9,4 +9,6 @@ def main(numbers1, numbers2):
     Returns:
         list: return answer
     """
-    return
+    numbers2.insert(0,(numbers1.pop(-1)))
+    return numbers2
+print(main([1,2,3,4,5,6,7],[8,9,10,11,12]))
